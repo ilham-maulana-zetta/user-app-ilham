27b707f42641cf096f70ca2cfb4582d2b079423a
M. Ilham Maulana <ilhambalcazar14@gmail.com>
2022-04-22 10:03:54 +0700

    change title and favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'user-app-ilham',
+    title: 'Users App | MyAOne',
     htmlAttrs: {
       lang: 'en'
     },
